2a2c728d488234c6657dafb7de7b604c5129cbcb
badyi <badyi&@icloud.com>
2019-12-19 12:39:34 +0300

    Initial Commit

---
+++ cutestDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cutestDogs
+//
+//  Created by Бадый Шагаалан on 19.12.2019.
+//  Copyright © 2019 badyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
